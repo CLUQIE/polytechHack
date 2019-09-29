c5720238fa6753212f90e9da4e0765246925e08a
Cluqie <zaytsevmiron@gmail.com>
2019-09-29 10:51:16 +0300

    Updates

---
+++ precache-manifest.5f4a129b45abebd6aa337cb69ab5acea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a7ac0112068ad5589d5414fb7d80bd",
+    "url": "/polytechHack/index.html"
+  },
+  {
+    "revision": "27eb901a52a301b0f873",
+    "url": "/polytechHack/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "ddcd00b1d58dce9141b3",
+    "url": "/polytechHack/static/css/main.cd73b641.chunk.css"
+  },
+  {
+    "revision": "27eb901a52a301b0f873",
+    "url": "/polytechHack/static/js/2.8de781d4.chunk.js"
+  },
+  {
+    "revision": "ddcd00b1d58dce9141b3",
+    "url": "/polytechHack/static/js/main.afc9f4c0.chunk.js"
+  },
+  {
+    "revision": "c6f797d5780a4dc00641",
+    "url": "/polytechHack/static/js/runtime-main.6b7cd6ad.js"
+  },
+  {
+    "revision": "3524a2cddef9155d6dd02e6c9123d9e6",
+    "url": "/polytechHack/static/media/card.3524a2cd.jpg"
+  },
+  {
+    "revision": "ab53249e54e6dbbd94e0aec5fbd24447",
+    "url": "/polytechHack/static/media/hack.ab53249e.jpg"
+  }
+]);
